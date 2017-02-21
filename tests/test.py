3b6c3383a24f4ebc047d78cfb61684ae7b064448
Randy LeVeque <rjl@uw.edu>
2017-02-21 13:55:27 -0800

    improve error message

--- tests/test.py
+++ tests/test.py
@@ -8,12 +8,17 @@ Sample test script.
 # use print function so this script works in Python 2 or 3:
 from __future__ import print_function
 
-x = 3.
+x = 1.
 y = 1.
 z = x + y
 expected_sum = 2.
 
-assert z==expected_sum, 'x = %g, y = %g, expect sum = %g' % (x,y,expected_sum)
+# informative error message in case test fails:
+error_message = 'x = %g, y = %g, x+y=%g disagrees with expected sum = %g' \
+    % (x,y,z,expected_sum)
 
-print("Runs fine!")
+# the real test:
+assert z==expected_sum, error_message
+
+print("Runs fine!")  # only if assert passes
 
