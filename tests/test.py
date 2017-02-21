a18cd518d8b669245462225217731326d27e5881
Randy LeVeque <rjl@uw.edu>
2017-02-21 13:42:23 -0800

    add print function

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
 
 """
 Sample test script.
-No output is produced if this runs properly.
 """
+
+# use print function so this script works in Python 2 or 3:
+from __future__ import print_function
 
 x = 1.
 y = 1.
@@ -10,4 +13,6 @@ z = x + y
 expected_sum = 2.
 
 assert z==expected_sum, 'x = %g, y = %g, expect sum = %g' % (x,y,expected_sum)
+
+print("Runs fine!")
 
