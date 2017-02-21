1b7ea77f2894c78b2cf6f1bf36afa5093ea22931
Randy LeVeque <rjl@uw.edu>
2017-02-21 13:38:54 -0800

    add comments to test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,13 @@
 
+"""
+Sample test script.
+No output is produced if this runs properly.
+"""
+
 x = 1.
 y = 1.
 z = x + y
 expected_sum = 2.
 
 assert z==expected_sum, 'x = %g, y = %g, expect sum = %g' % (x,y,expected_sum)
+
